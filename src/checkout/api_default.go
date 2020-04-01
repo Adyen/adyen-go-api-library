6de456217747c314178a1c6e59e511ea9511d0f5
deepu105 <d4udts@gmail.com>
2020-04-01 15:13:25 +0200

    Fix templates to use correct base path

--- src/checkout/api_default.go
+++ src/checkout/api_default.go
@@ -25,7 +25,7 @@ type Checkout common.Service
 /*
 PaymentLinksPost Creates a payment link.
 Creates a payment link to our hosted payment form where shoppers can pay. The list of payment methods presented to the shopper depends on the &#x60;currency&#x60; and &#x60;country&#x60; parameters sent in the request.  For more information, refer to [Pay by Link documentation](https://docs.adyen.com/checkout/pay-by-link#create-payment-links-through-api).
- * @param request CreatePaymentLinkRequest - reference of CreatePaymentLinkRequest). 
+ * @param request CreatePaymentLinkRequest - reference of CreatePaymentLinkRequest).
  * @param ctxs ..._context.Context - optional, for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 @return CreatePaymentLinkResponse
 */
@@ -41,7 +41,7 @@ func (a *Checkout) PaymentLinksPost(request *CreatePaymentLinkRequest, ctxs ..._
 	)
 
 	// create path and map variables
-	localVarPath := a.Client.Cfg.BasePath + "/paymentLinks"
+	localVarPath := a.BasePath() + "/paymentLinks"
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := _neturl.Values{}
 	localVarFormParams := _neturl.Values{}
@@ -91,8 +91,8 @@ func (a *Checkout) PaymentLinksPost(request *CreatePaymentLinkRequest, ctxs ..._
 
 	if localVarHTTPResponse.StatusCode >= 300 {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: localVarHTTPResponse.Status,
+			BodyBytes: localVarBody,
+			Err:       localVarHTTPResponse.Status,
 		}
 		if localVarHTTPResponse.StatusCode == 200 {
 			var v CreatePaymentLinkResponse
@@ -110,8 +110,8 @@ func (a *Checkout) PaymentLinksPost(request *CreatePaymentLinkRequest, ctxs ..._
 	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 	if err != nil {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: err.Error(),
+			BodyBytes: localVarBody,
+			Err:       err.Error(),
 		}
 		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
@@ -122,7 +122,7 @@ func (a *Checkout) PaymentLinksPost(request *CreatePaymentLinkRequest, ctxs ..._
 /*
 PaymentMethodsPost Returns available payment methods.
 Queries the available payment methods for a transaction based on the transaction context (like amount, country, and currency). Besides giving back a list of the available payment methods, the response also returns which input details you need to collect from the shopper (to be submitted to &#x60;/payments&#x60;).  Although we highly recommend using this endpoint to ensure you are always offering the most up-to-date list of payment methods, its usage is optional. You can, for example, also cache the &#x60;/paymentMethods&#x60; response and update it once a week.
- * @param request PaymentMethodsRequest - reference of PaymentMethodsRequest). 
+ * @param request PaymentMethodsRequest - reference of PaymentMethodsRequest).
  * @param ctxs ..._context.Context - optional, for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 @return PaymentMethodsResponse
 */
@@ -138,7 +138,7 @@ func (a *Checkout) PaymentMethodsPost(request *PaymentMethodsRequest, ctxs ..._c
 	)
 
 	// create path and map variables
-	localVarPath := a.Client.Cfg.BasePath + "/paymentMethods"
+	localVarPath := a.BasePath() + "/paymentMethods"
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := _neturl.Values{}
 	localVarFormParams := _neturl.Values{}
@@ -188,8 +188,8 @@ func (a *Checkout) PaymentMethodsPost(request *PaymentMethodsRequest, ctxs ..._c
 
 	if localVarHTTPResponse.StatusCode >= 300 {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: localVarHTTPResponse.Status,
+			BodyBytes: localVarBody,
+			Err:       localVarHTTPResponse.Status,
 		}
 		if localVarHTTPResponse.StatusCode == 200 {
 			var v PaymentMethodsResponse
@@ -207,8 +207,8 @@ func (a *Checkout) PaymentMethodsPost(request *PaymentMethodsRequest, ctxs ..._c
 	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 	if err != nil {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: err.Error(),
+			BodyBytes: localVarBody,
+			Err:       err.Error(),
 		}
 		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
@@ -219,7 +219,7 @@ func (a *Checkout) PaymentMethodsPost(request *PaymentMethodsRequest, ctxs ..._c
 /*
 PaymentSessionPost Creates a payment session.
 Provides the data object that can be used to start the Checkout SDK. To set up the payment, pass its amount, currency, and other required parameters. We use this to optimise the payment flow and perform better risk assessment of the transaction.  For more information, refer to [How it works](https://docs.adyen.com/checkout#howitworks).
- * @param request PaymentSetupRequest - reference of PaymentSetupRequest). 
+ * @param request PaymentSetupRequest - reference of PaymentSetupRequest).
  * @param ctxs ..._context.Context - optional, for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 @return PaymentSetupResponse
 */
@@ -235,7 +235,7 @@ func (a *Checkout) PaymentSessionPost(request *PaymentSetupRequest, ctxs ..._con
 	)
 
 	// create path and map variables
-	localVarPath := a.Client.Cfg.BasePath + "/paymentSession"
+	localVarPath := a.BasePath() + "/paymentSession"
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := _neturl.Values{}
 	localVarFormParams := _neturl.Values{}
@@ -285,8 +285,8 @@ func (a *Checkout) PaymentSessionPost(request *PaymentSetupRequest, ctxs ..._con
 
 	if localVarHTTPResponse.StatusCode >= 300 {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: localVarHTTPResponse.Status,
+			BodyBytes: localVarBody,
+			Err:       localVarHTTPResponse.Status,
 		}
 		if localVarHTTPResponse.StatusCode == 200 {
 			var v PaymentSetupResponse
@@ -304,8 +304,8 @@ func (a *Checkout) PaymentSessionPost(request *PaymentSetupRequest, ctxs ..._con
 	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 	if err != nil {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: err.Error(),
+			BodyBytes: localVarBody,
+			Err:       err.Error(),
 		}
 		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
@@ -316,7 +316,7 @@ func (a *Checkout) PaymentSessionPost(request *PaymentSetupRequest, ctxs ..._con
 /*
 PaymentsDetailsPost Submits details for a payment.
 Submits details for a payment created using &#x60;/payments&#x60;. This step is only needed when no final state has been reached on the &#x60;/payments&#x60; request (for example for 3D Secure, or when getting redirected back directly from a payment method using an app switch).  The exact details, which need to be sent to this endpoint, are always specified in the response of the associated &#x60;/payments&#x60; request.  In addition, the endpoint can be used to verify a &#x60;payload&#x60;, which is returned after coming back from the Checkout SDK or any of the redirect based methods on the Checkout API.
- * @param request DetailsRequest - reference of DetailsRequest). 
+ * @param request DetailsRequest - reference of DetailsRequest).
  * @param ctxs ..._context.Context - optional, for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 @return PaymentResponse
 */
@@ -332,7 +332,7 @@ func (a *Checkout) PaymentsDetailsPost(request *DetailsRequest, ctxs ..._context
 	)
 
 	// create path and map variables
-	localVarPath := a.Client.Cfg.BasePath + "/payments/details"
+	localVarPath := a.BasePath() + "/payments/details"
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := _neturl.Values{}
 	localVarFormParams := _neturl.Values{}
@@ -382,8 +382,8 @@ func (a *Checkout) PaymentsDetailsPost(request *DetailsRequest, ctxs ..._context
 
 	if localVarHTTPResponse.StatusCode >= 300 {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: localVarHTTPResponse.Status,
+			BodyBytes: localVarBody,
+			Err:       localVarHTTPResponse.Status,
 		}
 		if localVarHTTPResponse.StatusCode == 200 {
 			var v PaymentResponse
@@ -401,8 +401,8 @@ func (a *Checkout) PaymentsDetailsPost(request *DetailsRequest, ctxs ..._context
 	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 	if err != nil {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: err.Error(),
+			BodyBytes: localVarBody,
+			Err:       err.Error(),
 		}
 		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
@@ -413,7 +413,7 @@ func (a *Checkout) PaymentsDetailsPost(request *DetailsRequest, ctxs ..._context
 /*
 PaymentsPost Starts a transaction.
 Sends payment parameters (like amount, country, and currency) together with the input details collected from the shopper. The response returns the result of the payment request: * For some payment methods (e.g. Visa, Mastercard, and SEPA Direct Debits) you&#39;ll get a final state in the &#x60;resultCode&#x60; (e.g. &#x60;authorised&#x60; or &#x60;refused&#x60;). * For other payment methods, you&#39;ll receive &#x60;redirectShopper&#x60; as &#x60;resultCode&#x60; together with a &#x60;redirectUrl&#x60;. In this case, the shopper must finalize the payment on the page behind the &#x60;redirectUrl&#x60;.
- * @param request PaymentRequest - reference of PaymentRequest). 
+ * @param request PaymentRequest - reference of PaymentRequest).
  * @param ctxs ..._context.Context - optional, for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 @return PaymentResponse
 */
@@ -429,7 +429,7 @@ func (a *Checkout) PaymentsPost(request *PaymentRequest, ctxs ..._context.Contex
 	)
 
 	// create path and map variables
-	localVarPath := a.Client.Cfg.BasePath + "/payments"
+	localVarPath := a.BasePath() + "/payments"
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := _neturl.Values{}
 	localVarFormParams := _neturl.Values{}
@@ -479,8 +479,8 @@ func (a *Checkout) PaymentsPost(request *PaymentRequest, ctxs ..._context.Contex
 
 	if localVarHTTPResponse.StatusCode >= 300 {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: localVarHTTPResponse.Status,
+			BodyBytes: localVarBody,
+			Err:       localVarHTTPResponse.Status,
 		}
 		if localVarHTTPResponse.StatusCode == 200 {
 			var v PaymentResponse
@@ -498,8 +498,8 @@ func (a *Checkout) PaymentsPost(request *PaymentRequest, ctxs ..._context.Contex
 	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 	if err != nil {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: err.Error(),
+			BodyBytes: localVarBody,
+			Err:       err.Error(),
 		}
 		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
@@ -510,7 +510,7 @@ func (a *Checkout) PaymentsPost(request *PaymentRequest, ctxs ..._context.Contex
 /*
 PaymentsResultPost Verifies payment result.
 Verifies the payment result using the payload returned from the Checkout SDK.  For more information, refer to [How it works](https://docs.adyen.com/checkout#howitworks).
- * @param request PaymentVerificationRequest - reference of PaymentVerificationRequest). 
+ * @param request PaymentVerificationRequest - reference of PaymentVerificationRequest).
  * @param ctxs ..._context.Context - optional, for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
 @return PaymentVerificationResponse
 */
@@ -526,7 +526,7 @@ func (a *Checkout) PaymentsResultPost(request *PaymentVerificationRequest, ctxs 
 	)
 
 	// create path and map variables
-	localVarPath := a.Client.Cfg.BasePath + "/payments/result"
+	localVarPath := a.BasePath() + "/payments/result"
 	localVarHeaderParams := make(map[string]string)
 	localVarQueryParams := _neturl.Values{}
 	localVarFormParams := _neturl.Values{}
@@ -576,8 +576,8 @@ func (a *Checkout) PaymentsResultPost(request *PaymentVerificationRequest, ctxs 
 
 	if localVarHTTPResponse.StatusCode >= 300 {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: localVarHTTPResponse.Status,
+			BodyBytes: localVarBody,
+			Err:       localVarHTTPResponse.Status,
 		}
 		if localVarHTTPResponse.StatusCode == 200 {
 			var v PaymentVerificationResponse
@@ -595,8 +595,8 @@ func (a *Checkout) PaymentsResultPost(request *PaymentVerificationRequest, ctxs 
 	err = a.Client.Decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
 	if err != nil {
 		newErr := common.GenericOpenAPIError{
-			BodyBytes:  localVarBody,
-			Err: err.Error(),
+			BodyBytes: localVarBody,
+			Err:       err.Error(),
 		}
 		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
