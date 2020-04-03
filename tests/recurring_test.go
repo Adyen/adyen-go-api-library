c988a8adf45b4a10f4a120f54c3c10f41a2466a1
deepu105 <d4udts@gmail.com>
2020-04-03 11:37:16 +0200

    add test for recurring

--- tests/recurring_test.go
+++ tests/recurring_test.go
@@ -7,6 +7,7 @@
 package tests
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
@@ -24,33 +25,100 @@ func Test_Recurring(t *testing.T) {
 	godotenv.Load("./../.env")
 
 	var (
-		// APIKey = os.Getenv("ADYEN_API_KEY")
-		USER = os.Getenv("ADYEN_USER")
-		PASS = os.Getenv("ADYEN_PASSWORD")
+		APIKey          = os.Getenv("ADYEN_API_KEY")
+		MerchantAccount = os.Getenv("ADYEN_MERCHANT")
 	)
 
-	client := adyen.NewAPIClient(&common.Config{
-		Environment: "TEST",
-		Username:    USER,
-		Password:    PASS,
-	})
+	client := adyen.NewAPIClientWithAPIKey(APIKey, "TEST")
+	// client.GetConfig().Debug = true
+
 	t.Run("ListRecurringDetails", func(t *testing.T) {
 		t.Run("Create an API request that should fail", func(t *testing.T) {
 			res, httpRes, err := client.Recurring.ListRecurringDetailsPost(&recurring.RecurringDetailsRequest{})
 			require.NotNil(t, err)
 			require.NotNil(t, httpRes)
-			assert.Equal(t, "500 Internal Server Error", err.Error())
-			assert.Equal(t, 500, httpRes.StatusCode)
-			assert.Equal(t, "500 Internal Server Error", httpRes.Status)
+			assert.Equal(t, "403 Forbidden", err.Error())
+			assert.Equal(t, 403, httpRes.StatusCode)
+			assert.Equal(t, "403 Forbidden", httpRes.Status)
 			require.NotNil(t, res)
+			errBody := map[string]string{}
+			json.Unmarshal(err.(common.GenericOpenAPIError).Body(), &errBody)
+			assert.Equal(t, "Invalid Merchant Account", errBody["message"])
 		})
 		t.Run("Create an API request that should pass", func(t *testing.T) {
-			res, httpRes, err := client.Recurring.ListRecurringDetailsPost(&recurring.RecurringDetailsRequest{})
+			res, httpRes, err := client.Recurring.ListRecurringDetailsPost(&recurring.RecurringDetailsRequest{
+				MerchantAccount:  MerchantAccount,
+				ShopperReference: "4343553GFGFYFY4654654675765",
+				Recurring: &recurring.RecurringType{
+					Contract: "RECURRING",
+				},
+			})
 			require.Nil(t, err)
 			require.NotNil(t, httpRes)
 			assert.Equal(t, 200, httpRes.StatusCode)
 			assert.Equal(t, "200 OK", httpRes.Status)
 			require.NotNil(t, res)
+			assert.Empty(t, res.ShopperReference)
+			assert.Empty(t, res.Details)
+		})
+	})
+
+	t.Run("Disable", func(t *testing.T) {
+		t.Run("Create an API request that should fail", func(t *testing.T) {
+			res, httpRes, err := client.Recurring.DisablePost(&recurring.DisableRequest{
+				MerchantAccount:          MerchantAccount,
+				ShopperReference:         "4343553GFGFYFY4654654675765",
+				RecurringDetailReference: "8314442372419167",
+			})
+			require.NotNil(t, err)
+			require.NotNil(t, httpRes)
+			assert.Equal(t, "422 Unprocessable Entity", err.Error())
+			assert.Equal(t, 422, httpRes.StatusCode)
+			assert.Equal(t, "422 Unprocessable Entity", httpRes.Status)
+			require.NotNil(t, res)
+			errBody := map[string]string{}
+			json.Unmarshal(err.(common.GenericOpenAPIError).Body(), &errBody)
+			assert.Equal(t, "Contract not found", errBody["message"])
+		})
+	})
+
+	t.Run("ScheduleAccountUpdater", func(t *testing.T) {
+		t.Run("Create an API request that should fail", func(t *testing.T) {
+			res, httpRes, err := client.Recurring.ScheduleAccountUpdaterPost(&recurring.ScheduleAccountUpdaterRequest{
+				MerchantAccount: MerchantAccount,
+				Reference:       "4343553GFGFYFY4654654675765",
+				// RecurringDetailReference: "8314442372419167",
+			})
+			require.NotNil(t, err)
+			require.NotNil(t, httpRes)
+			assert.Equal(t, "422 Unprocessable Entity", err.Error())
+			assert.Equal(t, 422, httpRes.StatusCode)
+			assert.Equal(t, "422 Unprocessable Entity", httpRes.Status)
+			require.NotNil(t, res)
+			errBody := map[string]string{}
+			json.Unmarshal(err.(common.GenericOpenAPIError).Body(), &errBody)
+			assert.Equal(t, "Missing card or shopperReference & selectedRecurringDetailReference", errBody["message"])
+		})
+		t.Run("Create an API request that should fail with card", func(t *testing.T) {
+			res, httpRes, err := client.Recurring.ScheduleAccountUpdaterPost(&recurring.ScheduleAccountUpdaterRequest{
+				MerchantAccount: MerchantAccount,
+				Reference:       "4343553GFGFYFY4654654675765",
+				Card: &recurring.Card{
+					ExpiryMonth: "03",
+					ExpiryYear:  "2030",
+					HolderName:  "Adyen test",
+					Number:      "4111111111111111",
+				},
+			})
+			require.NotNil(t, err)
+			require.NotNil(t, httpRes)
+			assert.Equal(t, "422 Unprocessable Entity", err.Error())
+			assert.Equal(t, 422, httpRes.StatusCode)
+			assert.Equal(t, "422 Unprocessable Entity", httpRes.Status)
+			require.NotNil(t, res)
+			errBody := map[string]string{}
+			json.Unmarshal(err.(common.GenericOpenAPIError).Body(), &errBody)
+			assert.Equal(t, "validation 000 No registered account for AccountUpdater", errBody["message"])
 		})
 	})
 }
