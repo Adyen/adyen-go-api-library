f8226d4031ad4f3d5856f88f165dc1ab8ca6c221
Mykhailo Alipa <alipa.mikhail@gmail.com>
2022-07-12 10:23:24 +0200

    fix panic when NotificationItems is nil

--- src/notification/model_notification.go
+++ src/notification/model_notification.go
@@ -8,6 +8,11 @@ type Notification struct {
 // GetNotificationItems returns a reference to NotificationRequestItem's inside the NotificationItem array
 func (n *Notification) GetNotificationItems() []*NotificationRequestItem {
 	resp := []*NotificationRequestItem{}
+
+	if n.NotificationItems == nil {
+		return resp
+	}
+
 	for _, v := range *n.NotificationItems {
 		resp = append(resp, &v.NotificationRequestItem)
 	}
