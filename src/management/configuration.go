a89d9b477e1ca460501bc5d778428d85a561e0c2
gcatanese <gcatanese@yahoo.com>
2022-09-27 15:15:32 +0200

    Minor edits

--- src/management/configuration.go
+++ src/management/configuration.go
@@ -20,17 +20,17 @@ Contact: developer-experience@adyen.com
 package Management
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"strings"
+    "context"
+    "fmt"
+    "net/http"
+    "strings"
 )
 
 // Management API settings
 const (
-	ManagementApiVersion      = "v1"
-	ManagementApiEndpointTest = "https://management-test.adyen.com/" + ManagementApiVersion
-	ManagementApiEndpointLive = "https://management-live.adyen.com/" + ManagementApiVersion
+    ManagementApiVersion      = "v1"
+    ManagementApiEndpointTest = "https://management-test.adyen.com/" + ManagementApiVersion
+    ManagementApiEndpointLive = "https://management-live.adyen.com/" + ManagementApiVersion
 )
 
 // contextKeys are used to identify the type of value in the context.
@@ -40,62 +40,62 @@ const (
 type contextKey string
 
 func (c contextKey) String() string {
-	return "auth " + string(c)
+    return "auth " + string(c)
 }
 
 var (
-	// ContextOAuth2 takes an oauth2.TokenSource as authentication for the request.
-	ContextOAuth2 = contextKey("token")
+    // ContextOAuth2 takes an oauth2.TokenSource as authentication for the request.
+    ContextOAuth2 = contextKey("token")
 
-	// ContextBasicAuth takes BasicAuth as authentication for the request.
-	ContextBasicAuth = contextKey("basic")
+    // ContextBasicAuth takes BasicAuth as authentication for the request.
+    ContextBasicAuth = contextKey("basic")
 
-	// ContextAccessToken takes a string oauth2 access token as authentication for the request.
-	ContextAccessToken = contextKey("accesstoken")
+    // ContextAccessToken takes a string oauth2 access token as authentication for the request.
+    ContextAccessToken = contextKey("accesstoken")
 
-	// ContextAPIKeys takes a string apikey as authentication for the request
-	ContextAPIKeys = contextKey("apiKeys")
+    // ContextAPIKeys takes a string apikey as authentication for the request
+    ContextAPIKeys = contextKey("apiKeys")
 
-	// ContextHttpSignatureAuth takes HttpSignatureAuth as authentication for the request.
-	ContextHttpSignatureAuth = contextKey("httpsignature")
+    // ContextHttpSignatureAuth takes HttpSignatureAuth as authentication for the request.
+    ContextHttpSignatureAuth = contextKey("httpsignature")
 
-	// ContextServerIndex uses a server configuration from the index.
-	ContextServerIndex = contextKey("serverIndex")
+    // ContextServerIndex uses a server configuration from the index.
+    ContextServerIndex = contextKey("serverIndex")
 
-	// ContextOperationServerIndices uses a server configuration from the index mapping.
-	ContextOperationServerIndices = contextKey("serverOperationIndices")
+    // ContextOperationServerIndices uses a server configuration from the index mapping.
+    ContextOperationServerIndices = contextKey("serverOperationIndices")
 
-	// ContextServerVariables overrides a server configuration variables.
-	ContextServerVariables = contextKey("serverVariables")
+    // ContextServerVariables overrides a server configuration variables.
+    ContextServerVariables = contextKey("serverVariables")
 
-	// ContextOperationServerVariables overrides a server configuration variables using operation specific values.
-	ContextOperationServerVariables = contextKey("serverOperationVariables")
+    // ContextOperationServerVariables overrides a server configuration variables using operation specific values.
+    ContextOperationServerVariables = contextKey("serverOperationVariables")
 )
 
 // BasicAuth provides basic http authentication to a request passed via context using ContextBasicAuth
 type BasicAuth struct {
-	UserName string `json:"userName,omitempty"`
-	Password string `json:"password,omitempty"`
+    UserName string `json:"userName,omitempty"`
+    Password string `json:"password,omitempty"`
 }
 
 // APIKey provides API key based authentication to a request passed via context using ContextAPIKey
 type APIKey struct {
-	Key    string
-	Prefix string
+    Key    string
+    Prefix string
 }
 
 // ServerVariable stores the information about a server variable
 type ServerVariable struct {
-	Description  string
-	DefaultValue string
-	EnumValues   []string
+    Description  string
+    DefaultValue string
+    EnumValues   []string
 }
 
 // ServerConfiguration stores the information about a server
 type ServerConfiguration struct {
-	URL         string
-	Description string
-	Variables   map[string]ServerVariable
+    URL         string
+    Description string
+    Variables   map[string]ServerVariable
 }
 
 // ServerConfigurations stores multiple ServerConfiguration items
@@ -105,169 +105,169 @@ type ServerConfigurations []ServerConfiguration
 type Environment string
 
 const (
-	TestEnv Environment = "TEST"
-	LiveEnv Environment = "LIVE"
+    TestEnv Environment = "TEST"
+    LiveEnv Environment = "LIVE"
 )
 
-// Configuration stores the configuration of the API client
+// ManagementAPIConfiguration stores the configuration of the API client
 type ManagementAPIConfiguration struct {
-	ApiKey           string            `json:"apiKey,omitempty"`
-	MerchantAccount  string            `json:"merchantAccount,omitempty"`
-	Environment      Environment       `json:"environment,omitempty"`
-	Host             string            `json:"host,omitempty"`
-	Scheme           string            `json:"scheme,omitempty"`
-	DefaultHeader    map[string]string `json:"defaultHeader,omitempty"`
-	UserAgent        string            `json:"userAgent,omitempty"`
-	Debug            bool              `json:"debug,omitempty"`
-	Servers          ServerConfigurations
-	OperationServers map[string]ServerConfigurations
-	HTTPClient       *http.Client
+    ApiKey           string            `json:"apiKey,omitempty"`
+    MerchantAccount  string            `json:"merchantAccount,omitempty"`
+    Environment      Environment       `json:"environment,omitempty"`
+    Host             string            `json:"host,omitempty"`
+    Scheme           string            `json:"scheme,omitempty"`
+    DefaultHeader    map[string]string `json:"defaultHeader,omitempty"`
+    UserAgent        string            `json:"userAgent,omitempty"`
+    Debug            bool              `json:"debug,omitempty"`
+    Servers          ServerConfigurations
+    OperationServers map[string]ServerConfigurations
+    HTTPClient       *http.Client
 }
 
-// NewConfiguration returns a new Configuration object
+// NewManagementAPIConfiguration returns a new ManagementAPIConfiguration object
 func NewManagementAPIConfiguration(apiKey string, environment Environment) (*ManagementAPIConfiguration, error) {
-	managementApiUrl := ""
+    managementApiUrl := ""
 
-	if(apiKey == "") {
-		return nil, fmt.Errorf("apiKey is not provided")
-	}
+    if apiKey == "" {
+        return nil, fmt.Errorf("apiKey is not provided")
+    }
 
-	if environment == LiveEnv {
-		managementApiUrl = ManagementApiEndpointLive
-	} else if environment == TestEnv {
-		managementApiUrl = ManagementApiEndpointTest
-	} else {
-		return nil, fmt.Errorf("unsupported environment %s", environment)
-		
-	}
+    if environment == LiveEnv {
+        managementApiUrl = ManagementApiEndpointLive
+    } else if environment == TestEnv {
+        managementApiUrl = ManagementApiEndpointTest
+    } else {
+        return nil, fmt.Errorf("unsupported environment %s", environment)
 
-	cfg := &ManagementAPIConfiguration{
-		DefaultHeader: make(map[string]string),
-		UserAgent:     "OpenAPI-Generator/1.0.0/go",
-		Debug:         false,
-		Servers: ServerConfigurations{
-			{
-				URL:         managementApiUrl,
-				Description: "N/A",
-			},
-		},
-		OperationServers: map[string]ServerConfigurations{},
-		ApiKey:           apiKey,
-		Environment:      environment,
-	}
-	return cfg, nil
+    }
+
+    cfg := &ManagementAPIConfiguration{
+        DefaultHeader: make(map[string]string),
+        UserAgent:     "OpenAPI-Generator/1.0.0/go",
+        Debug:         false,
+        Servers: ServerConfigurations{
+            {
+                URL:         managementApiUrl,
+                Description: "N/A",
+            },
+        },
+        OperationServers: map[string]ServerConfigurations{},
+        ApiKey:           apiKey,
+        Environment:      environment,
+    }
+    return cfg, nil
 }
 
 // AddDefaultHeader adds a new HTTP header to the default header in the request
 func (c *ManagementAPIConfiguration) AddDefaultHeader(key string, value string) {
-	c.DefaultHeader[key] = value
+    c.DefaultHeader[key] = value
 }
 
 // URL formats template on a index using given variables
 func (sc ServerConfigurations) URL(index int, variables map[string]string) (string, error) {
-	if index < 0 || len(sc) <= index {
-		return "", fmt.Errorf("Index %v out of range %v", index, len(sc)-1)
-	}
-	server := sc[index]
-	url := server.URL
+    if index < 0 || len(sc) <= index {
+        return "", fmt.Errorf("Index %v out of range %v", index, len(sc)-1)
+    }
+    server := sc[index]
+    url := server.URL
 
-	// go through variables and replace placeholders
-	for name, variable := range server.Variables {
-		if value, ok := variables[name]; ok {
-			found := bool(len(variable.EnumValues) == 0)
-			for _, enumValue := range variable.EnumValues {
-				if value == enumValue {
-					found = true
-				}
-			}
-			if !found {
-				return "", fmt.Errorf("The variable %s in the server URL has invalid value %v. Must be %v", name, value, variable.EnumValues)
-			}
-			url = strings.Replace(url, "{"+name+"}", value, -1)
-		} else {
-			url = strings.Replace(url, "{"+name+"}", variable.DefaultValue, -1)
-		}
-	}
-	return url, nil
+    // go through variables and replace placeholders
+    for name, variable := range server.Variables {
+        if value, ok := variables[name]; ok {
+            found := bool(len(variable.EnumValues) == 0)
+            for _, enumValue := range variable.EnumValues {
+                if value == enumValue {
+                    found = true
+                }
+            }
+            if !found {
+                return "", fmt.Errorf("The variable %s in the server URL has invalid value %v. Must be %v", name, value, variable.EnumValues)
+            }
+            url = strings.Replace(url, "{"+name+"}", value, -1)
+        } else {
+            url = strings.Replace(url, "{"+name+"}", variable.DefaultValue, -1)
+        }
+    }
+    return url, nil
 }
 
 // ServerURL returns URL based on server settings
 func (c *ManagementAPIConfiguration) ServerURL(index int, variables map[string]string) (string, error) {
-	return c.Servers.URL(index, variables)
+    return c.Servers.URL(index, variables)
 }
 
 func getServerIndex(ctx context.Context) (int, error) {
-	si := ctx.Value(ContextServerIndex)
-	if si != nil {
-		if index, ok := si.(int); ok {
-			return index, nil
-		}
-		return 0, reportError("Invalid type %T should be int", si)
-	}
-	return 0, nil
+    si := ctx.Value(ContextServerIndex)
+    if si != nil {
+        if index, ok := si.(int); ok {
+            return index, nil
+        }
+        return 0, reportError("Invalid type %T should be int", si)
+    }
+    return 0, nil
 }
 
 func getServerOperationIndex(ctx context.Context, endpoint string) (int, error) {
-	osi := ctx.Value(ContextOperationServerIndices)
-	if osi != nil {
-		if operationIndices, ok := osi.(map[string]int); !ok {
-			return 0, reportError("Invalid type %T should be map[string]int", osi)
-		} else {
-			index, ok := operationIndices[endpoint]
-			if ok {
-				return index, nil
-			}
-		}
-	}
-	return getServerIndex(ctx)
+    osi := ctx.Value(ContextOperationServerIndices)
+    if osi != nil {
+        if operationIndices, ok := osi.(map[string]int); !ok {
+            return 0, reportError("Invalid type %T should be map[string]int", osi)
+        } else {
+            index, ok := operationIndices[endpoint]
+            if ok {
+                return index, nil
+            }
+        }
+    }
+    return getServerIndex(ctx)
 }
 
 func getServerVariables(ctx context.Context) (map[string]string, error) {
-	sv := ctx.Value(ContextServerVariables)
-	if sv != nil {
-		if variables, ok := sv.(map[string]string); ok {
-			return variables, nil
-		}
-		return nil, reportError("ctx value of ContextServerVariables has invalid type %T should be map[string]string", sv)
-	}
-	return nil, nil
+    sv := ctx.Value(ContextServerVariables)
+    if sv != nil {
+        if variables, ok := sv.(map[string]string); ok {
+            return variables, nil
+        }
+        return nil, reportError("ctx value of ContextServerVariables has invalid type %T should be map[string]string", sv)
+    }
+    return nil, nil
 }
 
 func getServerOperationVariables(ctx context.Context, endpoint string) (map[string]string, error) {
-	osv := ctx.Value(ContextOperationServerVariables)
-	if osv != nil {
-		if operationVariables, ok := osv.(map[string]map[string]string); !ok {
-			return nil, reportError("ctx value of ContextOperationServerVariables has invalid type %T should be map[string]map[string]string", osv)
-		} else {
-			variables, ok := operationVariables[endpoint]
-			if ok {
-				return variables, nil
-			}
-		}
-	}
-	return getServerVariables(ctx)
+    osv := ctx.Value(ContextOperationServerVariables)
+    if osv != nil {
+        if operationVariables, ok := osv.(map[string]map[string]string); !ok {
+            return nil, reportError("ctx value of ContextOperationServerVariables has invalid type %T should be map[string]map[string]string", osv)
+        } else {
+            variables, ok := operationVariables[endpoint]
+            if ok {
+                return variables, nil
+            }
+        }
+    }
+    return getServerVariables(ctx)
 }
 
 // ServerURLWithContext returns a new server URL given an endpoint
 func (c *ManagementAPIConfiguration) ServerURLWithContext(ctx context.Context, endpoint string) (string, error) {
-	sc, ok := c.OperationServers[endpoint]
-	if !ok {
-		sc = c.Servers
-	}
+    sc, ok := c.OperationServers[endpoint]
+    if !ok {
+        sc = c.Servers
+    }
 
-	if ctx == nil {
-		return sc.URL(0, nil)
-	}
+    if ctx == nil {
+        return sc.URL(0, nil)
+    }
 
-	index, err := getServerOperationIndex(ctx, endpoint)
-	if err != nil {
-		return "", err
-	}
+    index, err := getServerOperationIndex(ctx, endpoint)
+    if err != nil {
+        return "", err
+    }
 
-	variables, err := getServerOperationVariables(ctx, endpoint)
-	if err != nil {
-		return "", err
-	}
+    variables, err := getServerOperationVariables(ctx, endpoint)
+    if err != nil {
+        return "", err
+    }
 
-	return sc.URL(index, variables)
+    return sc.URL(index, variables)
 }
