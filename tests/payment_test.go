d42e5a6866b8e5d8bfe0b3c8f9cb6f114844647b
Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>
2023-04-17 08:24:45 +0000

    fix resultcode assertion for payments unit test

--- tests/payment_test.go
+++ tests/payment_test.go
@@ -87,11 +87,12 @@ func Test_Payment(t *testing.T) {
 		})
 
 		t.Run("Authorise", func(t *testing.T) {
+			resultCode := common.Authorised.String()
 			res, httpRes, err := authorisePost()
 
 			assertForSuccessResponse(res, httpRes, err)
 			assert.NotNil(t, res.PspReference)
-			assert.Equal(t, res.ResultCode, common.Authorised)
+			assert.Equal(t, res.ResultCode, &resultCode)
 		})
 
 		t.Run("GetAuthenticationResult", func(t *testing.T) {
