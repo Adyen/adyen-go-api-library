f1af4bbe076aee28da1f139db46eea0a23d0dc14
michaelpa <michael.paul@adyen.com>
2023-04-26 11:20:39 +0200

    ITT-84: Error case

--- tests/storedvalue_test.go
+++ tests/storedvalue_test.go
@@ -4,27 +4,23 @@ import (
 	"github.com/adyen/adyen-go-api-library/v6/src/adyen"
 	"github.com/adyen/adyen-go-api-library/v6/src/common"
 	"github.com/adyen/adyen-go-api-library/v6/src/storedvalue"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
-
-	"github.com/joho/godotenv"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func Test_StoredValue(t *testing.T) {
-	godotenv.Load("./../.env")
-
 	client := adyen.NewClient(&common.Config{
-		ApiKey:      os.Getenv("ADYEN_API_KEY"),
+		ApiKey:      "YOUR_ADYEN_API_KEY",
 		Environment: "TEST",
 	})
 	//client.GetConfig().Debug = true
+
 	mux := http.NewServeMux()
+	// Success case
 	mux.HandleFunc("/checkBalance", func(w http.ResponseWriter, r *http.Request) {
 		require.Equal(t, "POST", r.Method)
 		w.Header().Set("Content-Type", "application/json")
@@ -37,11 +33,17 @@ func Test_StoredValue(t *testing.T) {
     "resultCode": "Success"
 }`)
 	})
+	// Error case
+	mux.HandleFunc("/voidTransaction", func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, "POST", r.Method)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, `{"status":500,"errorCode":"306","message":"No voidable transaction found","errorType":"internal"}`)
+	})
+
 	mockServer := httptest.NewServer(mux)
 	defer mockServer.Close()
-	client.StoredValue.BasePath = func() string {
-		return mockServer.URL
-	}
+	client.StoredValue.BasePath = func() string { return mockServer.URL }
 
 	t.Run("Configuration", func(t *testing.T) {
 		testClient := adyen.NewClient(&common.Config{
@@ -71,5 +73,18 @@ func Test_StoredValue(t *testing.T) {
 		assert.Equal(t, "Success", res.GetResultCode())
 		balance := res.GetCurrentBalance()
 		assert.Equal(t, int64(5600), balance.GetValue())
+	})
+
+	t.Run("Error response", func(t *testing.T) {
+		_, httpRes, err := client.StoredValue.VoidTransaction(&storedvalue.StoredValueVoidRequest{
+			MerchantAccount:   "YOUR_MERCHANT_ACCOUNT",
+			OriginalReference: "",
+			Reference:         nil,
+		})
+
+		assert.Equal(t, 500, httpRes.StatusCode)
+		apiError := err.(common.APIError)
+		assert.Equal(t, "306", apiError.Code)
+		assert.Equal(t, "No voidable transaction found", apiError.Message)
 	})
 }
