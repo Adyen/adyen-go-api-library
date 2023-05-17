47690a6b4e0f6ec5c07fafc5944f5f770a285c96
Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>
2023-05-17 09:54:28 +0000

    fix unit tests

--- tests/transfers_test.go
+++ tests/transfers_test.go
@@ -1,30 +1,29 @@
 package tests
 
 import (
-	"github.com/adyen/adyen-go-api-library/v6/src/adyen"
-	"github.com/adyen/adyen-go-api-library/v6/src/common"
-	"github.com/adyen/adyen-go-api-library/v6/src/transfers"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"context"
 	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
-	"context"
+
+	"github.com/adyen/adyen-go-api-library/v6/src/adyen"
+	"github.com/adyen/adyen-go-api-library/v6/src/common"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_Transfers(t *testing.T) {
 	client := adyen.NewClient(&common.Config{
-		ApiKey: "YOUR_ADYEN_API_KEY",
+		ApiKey:      "YOUR_ADYEN_API_KEY",
 		Environment: "TEST",
 	})
 
 	service := client.Transfers
 
-	mux:= http.NewServeMux()
+	mux := http.NewServeMux()
 
 	// Success case
-	mux.HandleFunc("/transfers", func(w http.ResponseWriter, r.*http.Request) {
+	mux.HandleFunc("/transfers", func(w http.ResponseWriter, r *http.Request) {
 		require.Equal(t, "POST", r.Method)
 		w.Header().Set("Content-Type", "application/json")
 		io.WriteString(w, `{
@@ -61,7 +60,7 @@ func Test_Transfers(t *testing.T) {
 	mux.HandleFunc("/transactions", func(w http.ResponseWriter, r *http.Request) {
 		require.Equal(t, "GET", r.Method)
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusForbiddenError)
+		w.WriteHeader(http.StatusForbidden)
 		io.WriteString(w, `{
 			"type": "https://docs.adyen.com/errors/forbidden",
 			"title": "Forbidden",
@@ -76,8 +75,8 @@ func Test_Transfers(t *testing.T) {
 
 		_, httpRes, err := service.GetTransactions(request)
 
-			require.NoError(t, err)
-			assert.Equal(t, 200, httpRes.StatusCode)
-			require.Nil(t, err)
+		require.NoError(t, err)
+		assert.Equal(t, 200, httpRes.StatusCode)
+		require.Nil(t, err)
 	})
 }
