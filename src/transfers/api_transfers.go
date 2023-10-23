e458c22671221734de71b92c58b9e7dce5d23a96
michaelpa <michael.paul@adyen.com>
2023-10-23 10:53:04 +0200

    Update Transfers services

--- src/transfers/api_transfers.go
+++ src/transfers/api_transfers.go
@@ -70,6 +70,10 @@ func (a *TransfersApi) ReturnTransfer(ctx context.Context, r TransfersApiReturnT
 		headerParams,
 	)
 
+	if httpRes == nil {
+		return *res, httpRes, err
+	}
+
 	var serviceError common.RestServiceError
 
 	if httpRes.StatusCode == 401 {
