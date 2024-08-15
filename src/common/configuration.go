93cec5986159d4bcb9fa7597bb2cb7cc09fc5532
jillingk <93914435+jillingk@users.noreply.github.com>
2024-08-15 16:04:58 +0200

    Update configuration.go (#351)

--- src/common/configuration.go
+++ src/common/configuration.go
@@ -68,7 +68,7 @@ type Config struct {
 	DisputesEndpoint              string        `json:"disputesEndpoint,omitempty"`
 	BalancePlatformEndpoint       string        `json:"balancePlatformEndpoint,omitempty"`
 	ManagementEndpoint            string        `json:"managementEndpoint,omitempty"`
-	PaymentsAppEndpoint           string        `json:"managementEndpoint,omitempty"`
+	PaymentsAppEndpoint           string        `json:"paymentsAppEndpoint,omitempty"`
 	LegalEntityEndpoint           string        `json:"legalEntityEndpoint,omitempty"`
 	TransfersEndpoint             string        `json:"transfersEndpoint,omitempty"`
 	PosTerminalManagementEndpoint string        `json:"posTerminalManagementEndpoint,omitempty"`
