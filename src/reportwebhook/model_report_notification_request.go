b73943850454147927ce28520edb6a090a350f0a
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2025-12-23 08:54:09 +0000

    [adyen-sdk-automation] automated changes

--- src/reportwebhook/model_report_notification_request.go
+++ src/reportwebhook/model_report_notification_request.go
@@ -10,9 +10,8 @@ package reportwebhook
 
 import (
 	"encoding/json"
+    "github.com/adyen/adyen-go-api-library/v21/src/common"
 	"time"
-
-	"github.com/adyen/adyen-go-api-library/v21/src/common"
 )
 
 // checks if the ReportNotificationRequest type satisfies the MappedNullable interface at compile time
@@ -154,7 +153,7 @@ func (o *ReportNotificationRequest) SetType(v string) {
 }
 
 func (o ReportNotificationRequest) MarshalJSON() ([]byte, error) {
-	toSerialize, err := o.ToMap()
+	toSerialize,err := o.ToMap()
 	if err != nil {
 		return []byte{}, err
 	}
@@ -208,12 +207,14 @@ func (v *NullableReportNotificationRequest) UnmarshalJSON(src []byte) error {
 	return json.Unmarshal(src, &v.value)
 }
 
+
 func (o *ReportNotificationRequest) isValidType() bool {
-	var allowedEnumValues = []string{"balancePlatform.report.created"}
-	for _, allowed := range allowedEnumValues {
-		if o.GetType() == allowed {
-			return true
-		}
-	}
-	return false
+    var allowedEnumValues = []string{ "balancePlatform.report.created" }
+    for _, allowed := range allowedEnumValues {
+        if o.GetType() == allowed {
+            return true
+        }
+    }
+    return false
 }
+
