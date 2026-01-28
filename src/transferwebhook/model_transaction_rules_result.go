dedd710b3b1f58c9995c96c816ba6e70846eda2f
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-01-28 12:57:09 +0000

    [transferwebhooks] Automated update from Adyen/adyen-openapi@87ad29a

--- src/transferwebhook/model_transaction_rules_result.go
+++ src/transferwebhook/model_transaction_rules_result.go
@@ -10,8 +10,7 @@ package transferwebhook
 
 import (
 	"encoding/json"
-
-	"github.com/adyen/adyen-go-api-library/v21/src/common"
+    "github.com/adyen/adyen-go-api-library/v21/src/common"
 )
 
 // checks if the TransactionRulesResult type satisfies the MappedNullable interface at compile time
@@ -175,7 +174,7 @@ func (o *TransactionRulesResult) SetTriggeredTransactionRules(v []TransactionEve
 }
 
 func (o TransactionRulesResult) MarshalJSON() ([]byte, error) {
-	toSerialize, err := o.ToMap()
+	toSerialize,err := o.ToMap()
 	if err != nil {
 		return []byte{}, err
 	}
@@ -234,3 +233,6 @@ func (v *NullableTransactionRulesResult) UnmarshalJSON(src []byte) error {
 	v.isSet = true
 	return json.Unmarshal(src, &v.value)
 }
+
+
+
