d61fad4ba084412588787e1331acdc3e90b4a046
AdyenAutomationBot <AdyenAutomationBot 38424300+AdyenAutomationBot@users.noreply.github.com>
2026-01-28 12:57:40 +0000

    style(fmt): code formatted

--- src/configurationwebhook/model_bulk_address.go
+++ src/configurationwebhook/model_bulk_address.go
@@ -10,7 +10,8 @@ package configurationwebhook
 
 import (
 	"encoding/json"
-    "github.com/adyen/adyen-go-api-library/v21/src/common"
+
+	"github.com/adyen/adyen-go-api-library/v21/src/common"
 )
 
 // checks if the BulkAddress type satisfies the MappedNullable interface at compile time
@@ -473,7 +474,7 @@ func (o *BulkAddress) SetStreet(v string) {
 }
 
 func (o BulkAddress) MarshalJSON() ([]byte, error) {
-	toSerialize,err := o.ToMap()
+	toSerialize, err := o.ToMap()
 	if err != nil {
 		return []byte{}, err
 	}
@@ -557,6 +558,3 @@ func (v *NullableBulkAddress) UnmarshalJSON(src []byte) error {
 	v.isSet = true
 	return json.Unmarshal(src, &v.value)
 }
-
-
-
