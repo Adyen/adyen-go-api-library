ba43fbefc6ff6c3e4d57ebfcaa324190a3399661
AdyenAutomationBot <AdyenAutomationBot 38424300+AdyenAutomationBot@users.noreply.github.com>
2026-01-28 12:57:36 +0000

    style(fmt): code formatted

--- src/managementwebhook/model_terminal_assignment_notification_request.go
+++ src/managementwebhook/model_terminal_assignment_notification_request.go
@@ -10,7 +10,8 @@ package managementwebhook
 
 import (
 	"encoding/json"
-    "github.com/adyen/adyen-go-api-library/v21/src/common"
+
+	"github.com/adyen/adyen-go-api-library/v21/src/common"
 )
 
 // checks if the TerminalAssignmentNotificationRequest type satisfies the MappedNullable interface at compile time
@@ -214,7 +215,7 @@ func (o *TerminalAssignmentNotificationRequest) SetUniqueTerminalId(v string) {
 }
 
 func (o TerminalAssignmentNotificationRequest) MarshalJSON() ([]byte, error) {
-	toSerialize,err := o.ToMap()
+	toSerialize, err := o.ToMap()
 	if err != nil {
 		return []byte{}, err
 	}
@@ -271,6 +272,3 @@ func (v *NullableTerminalAssignmentNotificationRequest) UnmarshalJSON(src []byte
 	v.isSet = true
 	return json.Unmarshal(src, &v.value)
 }
-
-
-
