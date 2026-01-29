e64ebfcfb64bf5fea16dbb64c32c0770161baac2
dentych <dennis@understory.io>
2026-01-29 12:58:46 +0100

    fix: fix json tags omitempty in NotificationRequestItem

--- src/webhook/model_notification_request_item.go
+++ src/webhook/model_notification_request_item.go
@@ -3,14 +3,14 @@ package webhook
 import "time"
 
 type NotificationRequestItem struct {
-	AdditionalData      *map[string]interface{} `json:"additionalData,omitEmpty"`
+	AdditionalData      *map[string]interface{} `json:"additionalData,omitempty"`
 	Amount              Amount                  `json:"amount"`
 	EventCode           string                  `json:"eventCode"`
 	EventDate           *time.Time              `json:"eventDate"`
 	MerchantAccountCode string                  `json:"merchantAccountCode"`
 	MerchantReference   string                  `json:"merchantReference"`
-	Operations          []string                `json:"operations,omitEmpty"`
-	OriginalReference   string                  `json:"originalReference,omitEmpty"`
+	Operations          []string                `json:"operations,omitempty"`
+	OriginalReference   string                  `json:"originalReference,omitempty"`
 	PaymentMethod       string                  `json:"paymentMethod"`
 	PspReference        string                  `json:"pspReference"`
 	Reason              string                  `json:"reason"`
