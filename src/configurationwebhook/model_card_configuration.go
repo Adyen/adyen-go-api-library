722a883dc5e8e76ad380feb64a69d356edcf2bbf
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-10 13:43:19 +0000

    [configurationwebhooks] Automated update from Adyen/adyen-openapi@9c9bc16

--- src/configurationwebhook/model_card_configuration.go
+++ src/configurationwebhook/model_card_configuration.go
@@ -19,32 +19,32 @@ var _ common.MappedNullable = &CardConfiguration{}
 
 // CardConfiguration struct for CardConfiguration
 type CardConfiguration struct {
-	// Overrides the activation label design ID defined in the `configurationProfileId`. The activation label is attached to the card and contains the activation instructions.
+	// The activation label attached to the card that contains the activation instructions.  This field overrides the activation label design ID defined in the card configuration profile.
 	Activation *string `json:"activation,omitempty"`
 	// Your app's URL, if you want to activate cards through your app. For example, **my-app://ref1236a7d**. A QR code is created based on this URL, and is included in the carrier. Before you use this field, reach out to your Adyen contact to set up the QR code process.   Maximum length: 255 characters.
 	ActivationUrl *string      `json:"activationUrl,omitempty"`
 	BulkAddress   *BulkAddress `json:"bulkAddress,omitempty"`
-	// The ID of the card image. This is the image that will be printed on the full front of the card.
+	// The unique identifier of the card image. This image is printed on the full front of the card.
 	CardImageId *string `json:"cardImageId,omitempty"`
-	// Overrides the carrier design ID defined in the `configurationProfileId`. The carrier is the letter or packaging to which the card is attached.
+	// The letter or packaging to which the card is attached.  This field overrides the carrier design ID defined in the card configuration profile.
 	Carrier *string `json:"carrier,omitempty"`
-	// The ID of the carrier image. This is the image that will printed on the letter to which the card is attached.
+	// The unique identifier of the carrier image. This image is printed on the letter to which the card is attached.
 	CarrierImageId *string `json:"carrierImageId,omitempty"`
-	// The ID of the card configuration profile that contains the settings of the card. For example, the envelope and PIN mailer designs or the logistics company handling the shipment. All the settings in the profile are applied to the card, unless you provide other fields to override them.  For example, send the `shipmentMethod` to override the logistics company defined in the card configuration profile.
+	// The unique identifier of the card configuration profile that contains the settings that are applied to the card. For example, the envelope and PIN mailer designs or the logistics company handling the shipment.  You can override some of the existing settings in the configuration profile by providing the corresponding fields in the `configuration` object. For example, send the `shipmentMethod` to override the logistics company defined in the card configuration profile.
 	ConfigurationProfileId string `json:"configurationProfileId"`
-	// The three-letter [ISO-4217](https://en.wikipedia.org/wiki/ISO_4217) currency code of the card. For example, **EUR**.
+	// The three-letter [ISO-4217](https://en.wikipedia.org/wiki/ISO_4217) currency code of the card. For example, **EUR**.  This field overrides the existing currency setting on the card configuration profile.
 	Currency *string `json:"currency,omitempty"`
-	// Overrides the envelope design ID defined in the `configurationProfileId`.
+	// Overrides the envelope design ID defined in the card configuration profile.
 	Envelope *string `json:"envelope,omitempty"`
-	// Overrides the insert design ID defined in the `configurationProfileId`. An insert is any additional material, such as marketing materials, that are shipped together with the card.
+	// Any additional material, such as marketing material, that is shipped together with the card.  This field overrides the insert design ID defined in the card configuration profile.
 	Insert *string `json:"insert,omitempty"`
 	// The two-letter [ISO-639-1](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes) language code of the card. For example, **en**.
 	Language *string `json:"language,omitempty"`
-	// The ID of the logo image. This is the image that will be printed on the partial front of the card, such as a logo on the upper right corner.
+	// The unique identifier of the logo image. This image is printed on the partial front of the card, for example, a logo on the upper right corner.
 	LogoImageId *string `json:"logoImageId,omitempty"`
-	// Overrides the PIN mailer design ID defined in the `configurationProfileId`. The PIN mailer is the letter on which the PIN is printed.
+	// The letter on which the PIN of the card is printed.  This field overrides the PIN mailer design ID defined in the card configuration profile.
 	PinMailer *string `json:"pinMailer,omitempty"`
-	// Overrides the logistics company defined in the `configurationProfileId`.
+	// The logistics company that ships the card.  This field overrides the logistics company defined in the card configuration profile.
 	ShipmentMethod *string `json:"shipmentMethod,omitempty"`
 }
 
