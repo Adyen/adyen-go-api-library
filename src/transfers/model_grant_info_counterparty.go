4e447cbdef814db4e437de03b22b1a8b3b4b1f36
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-16 13:06:34 +0000

    [transfers] Automated update from Adyen/adyen-openapi@dcadca9

--- src/transfers/model_grant_info_counterparty.go
+++ src/transfers/model_grant_info_counterparty.go
@@ -14,70 +14,36 @@ import (
 	"github.com/adyen/adyen-go-api-library/v21/src/common"
 )
 
-// checks if the Counterparty type satisfies the MappedNullable interface at compile time
-var _ common.MappedNullable = &Counterparty{}
+// checks if the GrantInfoCounterparty type satisfies the MappedNullable interface at compile time
+var _ common.MappedNullable = &GrantInfoCounterparty{}
 
-// Counterparty struct for Counterparty
-type Counterparty struct {
-	// The identifier of the receiving account holder. The payout will default to the primary balance account of this account holder if no `balanceAccountId` is provided.
-	AccountHolderId *string `json:"accountHolderId,omitempty"`
+// GrantInfoCounterparty struct for GrantInfoCounterparty
+type GrantInfoCounterparty struct {
 	// The identifier of the balance account that belongs to the receiving account holder.
 	BalanceAccountId *string `json:"balanceAccountId,omitempty"`
 	// The identifier of the transfer instrument that belongs to the legal entity of the account holder.
 	TransferInstrumentId *string `json:"transferInstrumentId,omitempty"`
 }
 
-// NewCounterparty instantiates a new Counterparty object
+// NewGrantInfoCounterparty instantiates a new GrantInfoCounterparty object
 // This constructor will assign default values to properties that have it defined,
 // and makes sure properties required by API are set, but the set of arguments
 // will change when the set of required properties is changed
-func NewCounterparty() *Counterparty {
-	this := Counterparty{}
+func NewGrantInfoCounterparty() *GrantInfoCounterparty {
+	this := GrantInfoCounterparty{}
 	return &this
 }
 
-// NewCounterpartyWithDefaults instantiates a new Counterparty object
+// NewGrantInfoCounterpartyWithDefaults instantiates a new GrantInfoCounterparty object
 // This constructor will only assign default values to properties that have it defined,
 // but it doesn't guarantee that properties required by API are set
-func NewCounterpartyWithDefaults() *Counterparty {
-	this := Counterparty{}
+func NewGrantInfoCounterpartyWithDefaults() *GrantInfoCounterparty {
+	this := GrantInfoCounterparty{}
 	return &this
-}
-
-// GetAccountHolderId returns the AccountHolderId field value if set, zero value otherwise.
-func (o *Counterparty) GetAccountHolderId() string {
-	if o == nil || common.IsNil(o.AccountHolderId) {
-		var ret string
-		return ret
-	}
-	return *o.AccountHolderId
-}
-
-// GetAccountHolderIdOk returns a tuple with the AccountHolderId field value if set, nil otherwise
-// and a boolean to check if the value has been set.
-func (o *Counterparty) GetAccountHolderIdOk() (*string, bool) {
-	if o == nil || common.IsNil(o.AccountHolderId) {
-		return nil, false
-	}
-	return o.AccountHolderId, true
-}
-
-// HasAccountHolderId returns a boolean if a field has been set.
-func (o *Counterparty) HasAccountHolderId() bool {
-	if o != nil && !common.IsNil(o.AccountHolderId) {
-		return true
-	}
-
-	return false
-}
-
-// SetAccountHolderId gets a reference to the given string and assigns it to the AccountHolderId field.
-func (o *Counterparty) SetAccountHolderId(v string) {
-	o.AccountHolderId = &v
 }
 
 // GetBalanceAccountId returns the BalanceAccountId field value if set, zero value otherwise.
-func (o *Counterparty) GetBalanceAccountId() string {
+func (o *GrantInfoCounterparty) GetBalanceAccountId() string {
 	if o == nil || common.IsNil(o.BalanceAccountId) {
 		var ret string
 		return ret
@@ -87,7 +53,7 @@ func (o *Counterparty) GetBalanceAccountId() string {
 
 // GetBalanceAccountIdOk returns a tuple with the BalanceAccountId field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *Counterparty) GetBalanceAccountIdOk() (*string, bool) {
+func (o *GrantInfoCounterparty) GetBalanceAccountIdOk() (*string, bool) {
 	if o == nil || common.IsNil(o.BalanceAccountId) {
 		return nil, false
 	}
@@ -95,7 +61,7 @@ func (o *Counterparty) GetBalanceAccountIdOk() (*string, bool) {
 }
 
 // HasBalanceAccountId returns a boolean if a field has been set.
-func (o *Counterparty) HasBalanceAccountId() bool {
+func (o *GrantInfoCounterparty) HasBalanceAccountId() bool {
 	if o != nil && !common.IsNil(o.BalanceAccountId) {
 		return true
 	}
@@ -104,12 +70,12 @@ func (o *Counterparty) HasBalanceAccountId() bool {
 }
 
 // SetBalanceAccountId gets a reference to the given string and assigns it to the BalanceAccountId field.
-func (o *Counterparty) SetBalanceAccountId(v string) {
+func (o *GrantInfoCounterparty) SetBalanceAccountId(v string) {
 	o.BalanceAccountId = &v
 }
 
 // GetTransferInstrumentId returns the TransferInstrumentId field value if set, zero value otherwise.
-func (o *Counterparty) GetTransferInstrumentId() string {
+func (o *GrantInfoCounterparty) GetTransferInstrumentId() string {
 	if o == nil || common.IsNil(o.TransferInstrumentId) {
 		var ret string
 		return ret
@@ -119,7 +85,7 @@ func (o *Counterparty) GetTransferInstrumentId() string {
 
 // GetTransferInstrumentIdOk returns a tuple with the TransferInstrumentId field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *Counterparty) GetTransferInstrumentIdOk() (*string, bool) {
+func (o *GrantInfoCounterparty) GetTransferInstrumentIdOk() (*string, bool) {
 	if o == nil || common.IsNil(o.TransferInstrumentId) {
 		return nil, false
 	}
@@ -127,7 +93,7 @@ func (o *Counterparty) GetTransferInstrumentIdOk() (*string, bool) {
 }
 
 // HasTransferInstrumentId returns a boolean if a field has been set.
-func (o *Counterparty) HasTransferInstrumentId() bool {
+func (o *GrantInfoCounterparty) HasTransferInstrumentId() bool {
 	if o != nil && !common.IsNil(o.TransferInstrumentId) {
 		return true
 	}
@@ -136,11 +102,11 @@ func (o *Counterparty) HasTransferInstrumentId() bool {
 }
 
 // SetTransferInstrumentId gets a reference to the given string and assigns it to the TransferInstrumentId field.
-func (o *Counterparty) SetTransferInstrumentId(v string) {
+func (o *GrantInfoCounterparty) SetTransferInstrumentId(v string) {
 	o.TransferInstrumentId = &v
 }
 
-func (o Counterparty) MarshalJSON() ([]byte, error) {
+func (o GrantInfoCounterparty) MarshalJSON() ([]byte, error) {
 	toSerialize, err := o.ToMap()
 	if err != nil {
 		return []byte{}, err
@@ -148,11 +114,8 @@ func (o Counterparty) MarshalJSON() ([]byte, error) {
 	return json.Marshal(toSerialize)
 }
 
-func (o Counterparty) ToMap() (map[string]interface{}, error) {
+func (o GrantInfoCounterparty) ToMap() (map[string]interface{}, error) {
 	toSerialize := map[string]interface{}{}
-	if !common.IsNil(o.AccountHolderId) {
-		toSerialize["accountHolderId"] = o.AccountHolderId
-	}
 	if !common.IsNil(o.BalanceAccountId) {
 		toSerialize["balanceAccountId"] = o.BalanceAccountId
 	}
@@ -162,38 +125,38 @@ func (o Counterparty) ToMap() (map[string]interface{}, error) {
 	return toSerialize, nil
 }
 
-type NullableCounterparty struct {
-	value *Counterparty
+type NullableGrantInfoCounterparty struct {
+	value *GrantInfoCounterparty
 	isSet bool
 }
 
-func (v NullableCounterparty) Get() *Counterparty {
+func (v NullableGrantInfoCounterparty) Get() *GrantInfoCounterparty {
 	return v.value
 }
 
-func (v *NullableCounterparty) Set(val *Counterparty) {
+func (v *NullableGrantInfoCounterparty) Set(val *GrantInfoCounterparty) {
 	v.value = val
 	v.isSet = true
 }
 
-func (v NullableCounterparty) IsSet() bool {
+func (v NullableGrantInfoCounterparty) IsSet() bool {
 	return v.isSet
 }
 
-func (v *NullableCounterparty) Unset() {
+func (v *NullableGrantInfoCounterparty) Unset() {
 	v.value = nil
 	v.isSet = false
 }
 
-func NewNullableCounterparty(val *Counterparty) *NullableCounterparty {
-	return &NullableCounterparty{value: val, isSet: true}
+func NewNullableGrantInfoCounterparty(val *GrantInfoCounterparty) *NullableGrantInfoCounterparty {
+	return &NullableGrantInfoCounterparty{value: val, isSet: true}
 }
 
-func (v NullableCounterparty) MarshalJSON() ([]byte, error) {
+func (v NullableGrantInfoCounterparty) MarshalJSON() ([]byte, error) {
 	return json.Marshal(v.value)
 }
 
-func (v *NullableCounterparty) UnmarshalJSON(src []byte) error {
+func (v *NullableGrantInfoCounterparty) UnmarshalJSON(src []byte) error {
 	v.isSet = true
 	return json.Unmarshal(src, &v.value)
 }
