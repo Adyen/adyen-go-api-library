51c7d968ba1a17560a3b05a6b850f13240d9b6c0
Jeanderson Barros Candido <2225536+jeandersonbc@users.noreply.github.com>
2026-07-23 11:18:18 +0200

    fix: use hmac.Equals for signature comparison
    Hmac validation shouldn't be using plain string equals. This change also aligns Hmac signature
    verification across other Adyen libraries.

--- src/webhook/model_notification_request_item.go
+++ src/webhook/model_notification_request_item.go
@@ -3,16 +3,16 @@ package webhook
 import "time"
 
 type NotificationRequestItem struct {
-	AdditionalData      *map[string]interface{} `json:"additionalData,omitempty"`
-	Amount              Amount                  `json:"amount"`
-	EventCode           string                  `json:"eventCode"`
-	EventDate           *time.Time              `json:"eventDate"`
-	MerchantAccountCode string                  `json:"merchantAccountCode"`
-	MerchantReference   string                  `json:"merchantReference"`
-	Operations          []string                `json:"operations,omitempty"`
-	OriginalReference   string                  `json:"originalReference,omitempty"`
-	PaymentMethod       string                  `json:"paymentMethod"`
-	PspReference        string                  `json:"pspReference"`
-	Reason              string                  `json:"reason"`
-	Success             string                  `json:"success"`
+	AdditionalData      *map[string]string `json:"additionalData,omitempty"`
+	Amount              Amount             `json:"amount"`
+	EventCode           string             `json:"eventCode"`
+	EventDate           *time.Time         `json:"eventDate"`
+	MerchantAccountCode string             `json:"merchantAccountCode"`
+	MerchantReference   string             `json:"merchantReference"`
+	Operations          []string           `json:"operations,omitempty"`
+	OriginalReference   string             `json:"originalReference,omitempty"`
+	PaymentMethod       string             `json:"paymentMethod"`
+	PspReference        string             `json:"pspReference"`
+	Reason              string             `json:"reason"`
+	Success             string             `json:"success"`
 }
